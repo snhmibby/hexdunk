5594d726f9eaa624775a51f1b6036d655cdae658
Jur <jurjenm@yahoo.com>
2021-09-30 03:30:49 +0200

    selection for string-box

--- widget/hexWidget.go
+++ widget/hexWidget.go
@@ -27,7 +27,7 @@ func numHexDigits(addr int64) int {
 }
 
 func addrLabel(addr int64, nDigits int) string {
-	return fmt.Sprintf("%0*X: ", nDigits, addr)
+	return fmt.Sprintf("%0*X:", nDigits, addr)
 }
 
 type TextColor struct {
@@ -95,7 +95,12 @@ func (h *HexViewWidget) saveState() {
 func bytesPerLine(width, charwidth float32) int {
 	//to display 1 byte takes 4 characters: 2 for hexdump, 1 trailing space and 1 print
 	maxChars := int(width / (4 * charwidth))
+
+	//round to multiple of 4
 	maxChars -= maxChars % 4
+	if maxChars == 0 {
+		maxChars = 4
+	}
 	return maxChars
 }
 
@@ -135,63 +140,15 @@ func (h *HexViewWidget) handleKeys() {
 }
 
 func printByte(b byte) string {
-	r := rune(b)
-	if unicode.IsPrint(r) {
+	if unicode.IsGraphic(rune(b)) {
 		return string(b)
 	} else {
 		return "."
 	}
 }
-
-//TODO: use imgui columns?
-//Print the cursor to the widget
-func (h *HexViewWidget) printCursor(startOffset, printOffset float32) {
-	cursorBG := color.RGBA{R: 255, G: 100, B: 000, A: 255}
-
-	canvas := G.GetCanvas()
-	screenPos := G.GetCursorScreenPos()
-	cursor := h.state.cursor
-	cursorY := int((cursor / h.bytesPerLine) * int64(h.charHeight))
-
-	//cursor in hex dump
-	cursorX := int(startOffset) + int((cursor%h.bytesPerLine)*int64(3*h.charWidth))
-	pos := screenPos.Add(image.Pt(cursorX, cursorY))
-	rect := image.Pt(int(h.charWidth*2), int(h.charHeight))
-	canvas.AddRectFilled(pos, pos.Add(rect), cursorBG, 0, 0)
-
-	//cursor in string dump
-	cursorX = int(printOffset) + int((cursor%h.bytesPerLine)*int64(h.charWidth))
-	strPos := screenPos.Add(image.Pt(cursorX, cursorY))
-	strRect := image.Pt(int(h.charWidth), int(h.charHeight))
-	canvas.AddRectFilled(strPos, strPos.Add(strRect), cursorBG, 0, 0)
-}
-
 func (h *HexViewWidget) inSelection(addr int64) bool {
 	off, size := h.state.Selection()
 	return addr >= off && addr < off+size
-}
-
-func (h *HexViewWidget) printHexBG(addr int64) {
-	canvas := G.GetCanvas()
-	pos := G.GetCursorScreenPos()
-
-	//print cursor
-	if addr == h.state.cursor {
-		cursorBG := color.RGBA{R: 255, G: 100, B: 000, A: 255}
-		rect := image.Pt(int(h.charWidth*3), int(h.charHeight))
-		canvas.AddRectFilled(pos, pos.Add(rect), cursorBG, 0, 0)
-	}
-
-	//print selection
-	if h.inSelection(addr) {
-		selectionBG := color.RGBA{B: 200, A: 200}
-		sz := 3
-		if (addr+1)%h.bytesPerLine == 0 {
-			sz = 2
-		}
-		rect := image.Pt(int(h.charWidth)*sz, int(h.charHeight))
-		canvas.AddRectFilled(pos, pos.Add(rect), selectionBG, 0, 0)
-	}
 }
 
 func (h *HexViewWidget) updateSelection(addr int64) {
@@ -215,16 +172,51 @@ func (h *HexViewWidget) updateSelection(addr int64) {
 	}
 }
 
-//to be called from Build() function, prints hexdump of byte and handles keyclicks and such
-func (h *HexViewWidget) BuildHexCell(addr int64, b byte) {
-	var hex string
-	if addr == h.buffer.Size() {
-		hex = "   "
-	} else {
-		hex = fmt.Sprintf("%02X ", b)
+func (h *HexViewWidget) printStrBG(addr int64) {
+	canvas := G.GetCanvas()
+	pos := G.GetCursorScreenPos()
+
+	if addr == h.state.cursor {
+		cursorBG := color.RGBA{R: 255, G: 100, B: 000, A: 255}
+		rect := image.Pt(int(h.charWidth), int(h.charHeight))
+		canvas.AddRectFilled(pos, pos.Add(rect), cursorBG, 0, 0)
 	}
-	h.printHexBG(addr)
-	I.Text(hex)
+
+	if h.inSelection(addr) {
+		selectionBG := color.RGBA{B: 200, A: 100}
+		rect := image.Pt(int(h.charWidth), int(h.charHeight))
+		canvas.AddRectFilled(pos, pos.Add(rect), selectionBG, 0, 0)
+	}
+}
+
+func (h *HexViewWidget) printHexBG(addr int64) {
+	canvas := G.GetCanvas()
+	pos := G.GetCursorScreenPos()
+
+	if addr == h.state.cursor {
+		cursorBG := color.RGBA{R: 255, G: 100, B: 000, A: 255}
+		rect := image.Pt(int(h.charWidth*2), int(h.charHeight))
+		canvas.AddRectFilled(pos, pos.Add(rect), cursorBG, 0, 0)
+	}
+
+	if h.inSelection(addr) {
+		selectionBG := color.RGBA{B: 200, A: 100}
+		sz := 3
+		if (addr+1)%h.bytesPerLine == 0 {
+			sz = 2
+		}
+		rect := image.Pt(int(h.charWidth)*sz, int(h.charHeight))
+		canvas.AddRectFilled(pos, pos.Add(rect), selectionBG, 0, 0)
+	}
+}
+
+func (h *HexViewWidget) BuildCell(addr int64, txt string) {
+	if len(txt) == 1 {
+		h.printStrBG(addr)
+	} else {
+		h.printHexBG(addr)
+	}
+	I.Text(txt)
 
 	//handle click events
 	G.Event().OnClick(G.MouseButtonRight, func() {
@@ -249,75 +241,101 @@ func (h *HexViewWidget) BuildHexCell(addr int64, b byte) {
 	}).Build()
 }
 
+//to be called from Build() function, prints hexdump of byte and handles keyclicks and such
+func (h *HexViewWidget) BuildHexCell(addr int64, b byte) {
+	var hex string
+	if addr == h.buffer.Size() {
+		hex = "   "
+	} else {
+		hex = fmt.Sprintf("%02X ", b)
+	}
+	h.BuildCell(addr, hex)
+}
+
+//to be called from Build() function, prints readable interpretation of byte and handles keyclicks and such
+func (h *HexViewWidget) BuildStrCell(addr int64, b byte) {
+	str := printByte(b)
+	h.BuildCell(addr, str)
+}
+
 func (h *HexViewWidget) Build() {
-	I.PushStyleVarVec2(I.StyleVarFramePadding, I.Vec2{X: 0, Y: 0})
-	I.PushStyleVarVec2(I.StyleVarItemSpacing, I.Vec2{X: 0, Y: 0})
+	I.PushStyleVarVec2(I.StyleVarFramePadding, I.Vec2{})
+	I.PushStyleVarVec2(I.StyleVarItemSpacing, I.Vec2{})
+	I.PushStyleVarVec2(I.StyleVarCellPadding, I.Vec2{})
 
 	child := G.Child().Border(false).Layout(G.Custom(func() {
 		h.calcSizes()
 		h.handleKeys() //XXX should this be here or somewhere else??
 
-		/* the hexview has 3 columns, so to speak,
-		 * column 1 is the address, column 2 is a hexadecimal dump of bytes, column 3 is a readable string
-		 * TODO: Imgui has a provision for printing text in a column. use it.
-		 *
-		 */
-		maxAddr := numHexDigits(h.buffer.Size())                           //saved for printing address
-		startOffset, _ := G.CalcTextSize(addrLabel(0, maxAddr))            //x-position of column 2
-		printOffset := startOffset + h.charWidth*float32(h.bytesPerLine)*3 //x-position of column 3
+		maxAddr := numHexDigits(h.buffer.Size())                //saved for printing address
+		addressSize, _ := G.CalcTextSize(addrLabel(0, maxAddr)) //x-position of column 2
 
-		//h.printCursor(startOffset, printOffset)
+		flags := I.TableFlags_BordersInnerV | I.TableFlags_BordersOuter | I.TableFlags_SizingFixedFit
+		if I.BeginTable("HexDumpTable", 3, flags, I.Vec2{}, 0) {
+			I.TableSetupColumn("Offset", 0, addressSize, 0)
+			I.TableSetupColumn("HexDump", 0, 3*h.charWidth*float32(h.bytesPerLine), 0)
+			I.TableSetupColumn("Readable", 0, float32(h.bytesPerLine)*h.charWidth, 0)
 
-		//print the hex dump using a listclipper
-		numLines := (h.buffer.Size() + h.bytesPerLine - 1) / h.bytesPerLine
-		lineBuffer := make([]byte, int(h.bytesPerLine)) //buffer to read the bytes for 1 line
-		var clip I.ListClipper
-		//dumb hack: do numlines + 10 because on big files, the last few lines get chopped off
-		//due to floating point errors in scrolling calculations
-		clip.BeginV(int(numLines+10), h.charHeight)
-		for clip.Step() {
-			for lnum := clip.DisplayStart; lnum < clip.DisplayEnd; lnum++ {
-				//read data for this line
-				offs := int64(lnum) * h.bytesPerLine
-				h.buffer.Seek(offs, io.SeekStart)
-				n, e := h.buffer.Read(lineBuffer)
-				if e != nil {
-					panic(e)
-				}
-				if n < 0 {
-					break
-				}
-
-				//column 1, address
-				I.Text(addrLabel(offs, maxAddr))
-
-				//column2, hex dump
-				for i := 0; i < n; i++ {
-					I.SameLineV(startOffset+h.charWidth*float32(i)*3, 0)
-					h.BuildHexCell(int64(lnum)*h.bytesPerLine+int64(i), lineBuffer[i])
-				}
-				//allow to select EOF
-				if n != int(h.bytesPerLine) {
-					I.SameLineV(startOffset+h.charWidth*float32(n)*3, 0)
-					h.BuildHexCell(h.buffer.Size(), 0)
-				}
-
-				//column3, readable string
-				for i := 0; i < n; i++ {
-					if !unicode.IsGraphic(rune(lineBuffer[i])) {
-						lineBuffer[i] = '.'
+			//print the hex dump using a listclipper
+			numLines := (h.buffer.Size() + h.bytesPerLine - 1) / h.bytesPerLine
+			lineBuffer := make([]byte, int(h.bytesPerLine)) //buffer to read the bytes for 1 line
+			var clip I.ListClipper
+			//dumb hack: do numlines + 10 because on big files, the last few lines get chopped off
+			//due to floating point errors in scrolling calculations
+			clip.BeginV(int(numLines+10), h.charHeight)
+			for clip.Step() {
+				for lnum := clip.DisplayStart; lnum < clip.DisplayEnd; lnum++ {
+					offs := int64(lnum) * h.bytesPerLine
+					if offs > h.buffer.Size() {
+						break
 					}
-					I.SameLineV(printOffset+float32(i)*h.charWidth, 0)
-					I.Text(string(lineBuffer[i : i+1]))
+
+					//read data for this line
+					h.buffer.Seek(offs, io.SeekStart)
+					n, e := h.buffer.Read(lineBuffer)
+					if n < 0 || e != nil {
+						//TODO: properly handle EOF here!!!
+						break
+					}
+
+					//address
+					I.TableNextColumn()
+					I.Text(addrLabel(offs, maxAddr))
+
+					//hex dump
+					I.TableNextColumn()
+					for i := 0; i < n; i++ {
+						if i != 0 {
+							I.SameLine()
+						}
+						h.BuildHexCell(offs+int64(i), lineBuffer[i])
+					}
+					//allow to select EOF??
+					if n != int(h.bytesPerLine) {
+						if n != 0 {
+							I.SameLine()
+						}
+						h.BuildHexCell(h.buffer.Size(), 0)
+					}
+
+					//readable string
+					I.TableNextColumn()
+					for i := 0; i < n; i++ {
+						if i != 0 {
+							I.SameLine()
+						}
+						h.BuildStrCell(offs+int64(i), lineBuffer[i])
+					}
 				}
 			}
+			clip.End()
 		}
-		clip.End()
+		I.EndTable()
 	}))
 
 	child.Build()
 
-	I.PopStyleVarV(2) //framepadding & itemspacing
+	I.PopStyleVarV(3)
 }
 
 func (h *HexViewWidget) clampAddr(a *int64) {
